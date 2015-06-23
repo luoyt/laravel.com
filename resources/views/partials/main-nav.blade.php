b227f3934db09d7f6b9747094b3c3b99ab20c0f3
artiship <lichuanliang@126.com>
2015-06-23 12:20:07 +0800

    change QQ

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,4 +1,4 @@
-<li class="nav-lumen"><a href="#">QQ群 201793588</a></li>
+<li class="nav-lumen"><a href="#">QQ群 297473872</a></li>
 <li class="nav-laracasts"><a href="#">视频教程</a></li>
 <li class="nav-lumen"><a target="_blank" href="https://github.com/laragirl/docs">参与翻译</a></li>
 
